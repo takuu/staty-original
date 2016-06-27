c861eb2592ff8e3505259693aba9cdeb104da1f1
takuu <taku30@gmail.com>
2016-06-26 22:22:52 -0700

    Reworking the League List styles

--- app/components/LeagueList/LeagueList.js
+++ app/components/LeagueList/LeagueList.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
+if (process.env.BROWSER) require('./styles.css');
 
-const League = ({leagues}) => {
+const League = ({ leagues }) => {
   return (
-    <div className='LeaguePage' style={{marginTop: '80px'}}>
+    <div className='LeaguePage' style={{ marginTop: '80px' }}>
       <div className='LeaguePage-container'>
         <h1>League</h1>
         {
           _.map(leagues, (league) => {
             return (
-              <div className='col-md-10 col-xs-10' key={league._id}>
-                <div className='col-md-7'>
-                  <Link to={`/${league.name}`}>
-                    {league.displayName || league.name}
+              <div className='col-md-6 col-xs-6' key={league._id}>
+                <div className='list-group'>
+                  <Link to={`/${league.name}`} className='list-group-item'>
+                    <div className='col-md-3'>
+                      <div className='league-img'></div>
+
+                    </div>
+                    <div className='col-md-6'>{league.displayName || league.name}</div>
+                    <div className='col-md-3'>
+                      {league.location}
+                    </div>
                   </Link>
-                </div>
-                <div className='col-md-5'>
-                  {league.location}
                 </div>
               </div>
             );
--- app/containers/LeagueListPage.js
+++ app/containers/LeagueListPage.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import LeagueList from '../components/LeagueList/LeagueList';
+import GoogleMap from 'google-map-react';
 import {
   getAllLeagues
 } from '../actions/leagues';
@@ -12,7 +13,10 @@ import {
 })
 export default class LeagueListRoute extends React.Component {
   static propTypes = {
-    leagues: PropTypes.array.isRequired
+    leagues: PropTypes.array.isRequired,
+    center: {lat: 59.938043, lng: 30.337157},
+    zoom: 9,
+    greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
   };
 
   static fillStore (redux) {
@@ -20,12 +24,21 @@ export default class LeagueListRoute extends React.Component {
   }
 
   render () {
-    const leagues = this.props.leagues;
+    const { leagues } = this.props;
 
     return (
-      <LeagueList
-        leagues={leagues}
-      />
+      <div>
+        <LeagueList leagues={leagues} />
+        <div style={{'backgroundColor': '#fff', 'width': '100%', 'height': '500px'}}></div>
+        <GoogleMap
+          apiKey={'Browser key 1'}
+          center={{lat: 59.938043, lng: 30.337157}}
+          zoom={9}>
+
+        </GoogleMap>
+
+      </div>
+
     );
   }
 }
