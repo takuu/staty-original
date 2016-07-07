d0e2b7d2f6c926910a7ed6c15207a05a6a02361b
takuu <taku30@gmail.com>
2016-07-07 09:34:29 -0700

    Sanity checkin to demo to Eric

--- app/components/core/PlayerScoreboard/PlayerScoreboard.js
+++ app/components/core/PlayerScoreboard/PlayerScoreboard.js
@@ -5,10 +5,10 @@ import _ from 'lodash';
 import statParser from '../../../utils/statParser';
 import helpers from '../../../utils/helpers';
 
-const HighStats = ({highs, title}) => {
-  const {assists, points, blocks, rebounds, steals} = highs;
+const PlayerScoreboard = ({highs, title}) => {
+  // const {assists, points, blocks, rebounds, steals} = highs;
 
-  let foo = (
+  return (
     <div className='high-container'>
       <div className='title'>{title || 'Season Highs'}</div>
       <ul className='list-group'>
@@ -28,32 +28,13 @@ const HighStats = ({highs, title}) => {
     </div>
 
   );
-  return (
-    <div>
-      <div className='title'>{title || 'Season Highs'}</div>
-      <div className='high-container'>
-      {
-        _.map(highs, (item, key) => {
-          const title = helpers.camelCaseToTitle(key);
-          return (
-            <div className='high-block'>
-              <div className='high-title'>{title}</div>
-              <div className='high-stat'>{item}</div>
-            </div>
-          );
-        })
-      }
-        </div>
-    </div>
-  );
-
 };
-HighStats.propTypes = {
+PlayerScoreboard.propTypes = {
   highs: PropTypes.object
 };
 
-HighStats.defaultProps = {
+PlayerScoreboard.defaultProps = {
   highs: {}
 };
 
-export default HighStats;
+export default PlayerScoreboard;
